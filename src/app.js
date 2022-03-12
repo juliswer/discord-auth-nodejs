e71db38048bcbb8d7a1328caf31ac6554f6e7b74
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 12:45:51 -0300

    updating the routes

--- src/app.js
+++ src/app.js
@@ -14,4 +14,6 @@ app.use("/", require("./routes/index.routes"));
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/dashboard", require("./routes/dashboard.routes"));
 
+//GLOBAL VARIABLES
+
 module.exports = app;
