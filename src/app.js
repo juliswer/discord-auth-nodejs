c68dff813b32e163c7cec8f513f3bb602b357cbb
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 12:45:19 -0300

    updating the routes

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const session = require('express-session');
 
 // SETINGS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// MIDDLEWARES
+
+// ROUTES
 app.use("/", require("./routes/index.routes"));
 app.use("/auth", require("./routes/auth.routes"));
 app.use("/dashboard", require("./routes/dashboard.routes"));
