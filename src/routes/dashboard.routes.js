92723ce0abc099b516104f078e1eec180138102d
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 15:32:45 -0300

    dashboard page

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -2,7 +2,9 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  res.render('dashboard')
+  res.render('dashboard', {
+    guilds: req.user.guilds
+  })
 });
 
 module.exports = router;
