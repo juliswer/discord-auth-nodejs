8f386376e9131ba22f1c28b915d45e89950b0234
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 12:41:10 -0300

    files

--- src/app.js
+++ src/app.js
@@ -2,11 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use("/", require("./routes/index.routes"));
-
 app.use("/auth", require("./routes/auth.routes"));
-
-app.get("/dashboard", (req, res) => {
-  res.send("dashboard");
-});
+app.use("/dashboard", require("./routes/dashboard.routes"));
 
 module.exports = app;
