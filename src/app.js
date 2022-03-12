f48f7724542c8389dcf146e1255b85cf00161d48
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 13:47:47 -0300

    updating the configfile

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const app = express();
 const session = require('express-session');
 
+require('./strategies/discordStrategy');
+
 // SETINGS
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
---
+++ src/strategies/discordStrategy.js
@@ -0,0 +1,3 @@
+const { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } = require("../config");
+
+console.log(DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET);
