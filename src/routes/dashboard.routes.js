daa5b680ea5a69b40a5eb699675b2caf2d3359b5
Julian Swerdlin <julianswer36@gmail.com>
2022-03-12 15:27:01 -0300

    configuring passport

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const router = Router();
 
 router.get("/", (req, res) => {
-  console.log(req.user);
+  res.send('dashboard')
 });
 
 module.exports = router;
